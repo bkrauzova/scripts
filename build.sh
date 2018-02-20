f837a5079f0869f4b8578b5a290fe0ffd7c1f29d
bstepankova <bstepankova@volny.cz>
2018-02-20 16:01:58 +0100

    Problem: backup
    Solution:
    
    Signed-off-by: Barbora Stepankova <BarboraStepankova@eaton.com>

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 set -e
 
 SRC=$1
+IAM=`pwd`
 
 usage ()
 {
@@ -9,14 +10,14 @@ usage ()
     echo "ext=c/c++"
 }
 
-if [ -z $SRC ]; then
+if [ -z $IAM/$SRC ]; then
     echo -e "source file missing...\n"
     usage
     exit 1
 fi
 
 # am I c++ code?
-if  echo $SRC | grep -q ".cc" ; then
+if  echo $IAM/$SRC | grep -q ".cc" ; then
     EXT=3;
 else
     # assuming .c here
@@ -26,16 +27,16 @@ fi
 LEN=${#SRC}
 OUT=${SRC:0:$LEN-$EXT} # substr
 
-if [ -e $OUT ]; then
-    rm $OUT
+if [ -e $IAM/$OUT ]; then
+    rm $IAM/$OUT
 fi
 
 if [ $EXT -eq 3 ]; then
-    g++ -std=c++11 $SRC -lcxxtools -lczmq -ltntdb  -o $OUT && ./$OUT
+    g++ -std=c++11 $IAM/$SRC -lcxxtools -lczmq -ltntdb  -o $IAM/$OUT && $IAM/$OUT
 fi
 
 if [ $EXT -eq 2 ]; then
-    gcc -std=c99 -D_SVID_SOURCE -Werror -ggdb -lczmq -lmlm $SRC -o $OUT && ./$OUT
+    gcc -std=c99 -D_SVID_SOURCE -Werror -ggdb -lczmq -lmlm $IAM/$SRC -o $IAM/$OUT && $IAM/$OUT
 fi
 
 exit 0
